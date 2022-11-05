0ef71390707a43ab35a59ae7169892bed413002d
Autovw <68228006+Autovw@users.noreply.github.com>
2022-11-05 21:10:09 +0100

    ✨ Set up item registry

--- src/main/java/haloofblocks/projectarsenal/core/registry/ArsenalItems.java
+++ src/main/java/haloofblocks/projectarsenal/core/registry/ArsenalItems.java
@@ -1,7 +1,10 @@
 package haloofblocks.projectarsenal.core.registry;
 
+import com.mrcrayfish.guns.GunMod;
+import com.mrcrayfish.guns.item.GunItem;
 import haloofblocks.projectarsenal.ProjectArsenal;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -11,4 +14,16 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ArsenalItems
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ProjectArsenal.MOD_ID);
+
+    // ===== GUNS ===== //
+    public static final RegistryObject<Item> AA_TWELVE = registerGun("aa12");
+
+    // ===== ATTACHMENTS ===== //
+
+    // ===== AMMUNITION ===== //
+
+    private static RegistryObject<Item> registerGun(String name)
+    {
+        return ITEMS.register(name, () -> new GunItem(new Item.Properties().stacksTo(1).tab(GunMod.GROUP)));
+    }
 }
