c2db9ead8682faef8af96b93fdfb648120d4180b
Gabriel <64159930+CasualNoLifer@users.noreply.github.com>
2022-11-05 16:01:24 -0500

    Registered sounds

--- src/main/java/haloofblocks/projectarsenal/core/registry/ArsenalSounds.java
+++ src/main/java/haloofblocks/projectarsenal/core/registry/ArsenalSounds.java
@@ -16,6 +16,14 @@ public class ArsenalSounds
 
     public static final RegistryObject<SoundEvent> AA_TWELVE = register("item.aa12.fire");
     public static final RegistryObject<SoundEvent> AUTO_NINE = register("item.auto9.fire");
+    public static final RegistryObject<SoundEvent> DESERT_EAGLE = register("item.desert_eagle.fire");
+    public static final RegistryObject<SoundEvent> DP_TWENTY_SEVEN = register("item.dp27.fire");
+    public static final RegistryObject<SoundEvent> GOLDEN_HAWK = register("item.golden_hawk.fire");
+    public static final RegistryObject<SoundEvent> M_TWO_FOUR_NINE = register("item.m249.fire");
+    public static final RegistryObject<SoundEvent> MAXIM_NINE = register("item.maxim9.fire");
+    public static final RegistryObject<SoundEvent> MOSSOU = register("item.mossou.fire");
+    public static final RegistryObject<SoundEvent> MP_FIVE_A_FOUR = register("item.mp5a4.fire");
+    public static final RegistryObject<SoundEvent> VAL = register("item.val.fire");
 
     /**
      * Helper method used to register sounds
