5d2060873f1e954906a4f1506190abf07468b601
Autovw <68228006+Autovw@users.noreply.github.com>
2022-12-10 10:10:36 +0100

    🐛 Fix semi-auto fire mode not being properly applied

--- src/main/java/haloofblocks/projectarsenal/common/event/FireModesHandler.java
+++ src/main/java/haloofblocks/projectarsenal/common/event/FireModesHandler.java
@@ -100,7 +100,7 @@ public class FireModesHandler
             gunTag.put("General", generalTag);
 
         // return if fire mode tag is already properly set
-        if (generalTag.getBoolean("Auto") == auto)
+        if (generalTag.contains("Auto") && generalTag.getBoolean("Auto") == auto)
             return;
 
         if (generalTag.contains("Auto", Tag.TAG_ANY_NUMERIC))
