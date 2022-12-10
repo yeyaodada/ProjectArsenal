46b1edaa81162e61c9564af7f88c8b2dfc9782ac
Autovw <68228006+Autovw@users.noreply.github.com>
2022-12-10 14:31:49 +0100

    ✨ Play sound effect if gun is fired in safety mode

---
+++ src/main/java/haloofblocks/projectarsenal/client/event/ClientSafetyModeHandler.java
@@ -0,0 +1,45 @@
+package haloofblocks.projectarsenal.client.event;
+
+import haloofblocks.projectarsenal.ProjectArsenal;
+import haloofblocks.projectarsenal.common.FireModes;
+import haloofblocks.projectarsenal.common.item.ArsenalGunItem;
+import haloofblocks.projectarsenal.core.registry.ArsenalSounds;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
+
+/**
+ * @author Autovw
+ */
+@Mod.EventBusSubscriber(modid = ProjectArsenal.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ClientSafetyModeHandler
+{
+    @SubscribeEvent
+    public static void onMouseButtonEvent(InputEvent.MouseInputEvent event)
+    {
+        Minecraft mc = Minecraft.getInstance();
+        Player player = mc.player;
+
+        if (player == null)
+            return;
+
+        ItemStack stack = player.getMainHandItem();
+
+        if (!(stack.getItem() instanceof ArsenalGunItem gunItem) || !gunItem.hasFireMode())
+            return;
+
+        if (!gunItem.getSelectedFireMode().equals(FireModes.SAFETY))
+            return;
+
+        if (mc.options.keyAttack.isDown() && event.getAction() == GLFW.GLFW_PRESS)
+        {
+            // Play sound effect when player attempts to fire gun in safety mode
+            player.playSound(ArsenalSounds.GUNFIRE_BLOCKED.get(), 1.0f, 1.0f);
+        }
+    }
+}
