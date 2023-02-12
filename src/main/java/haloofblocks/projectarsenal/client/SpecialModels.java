c8d08dedbd777182f1af21c386df783b2953ed3b
Autovw <68228006+Autovw@users.noreply.github.com>
2023-02-12 11:20:10 +0100

    🚧 Set up special model format

---
+++ src/main/java/haloofblocks/projectarsenal/client/SpecialModels.java
@@ -0,0 +1,59 @@
+package haloofblocks.projectarsenal.client;
+
+import haloofblocks.projectarsenal.ProjectArsenal;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ForgeModelBakery;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author Autovw
+ */
+@Mod.EventBusSubscriber(modid = ProjectArsenal.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public enum SpecialModels
+{
+    AA_TWELVE("gun/aa12"),
+    AUTO_NINE("gun/auto9"),
+    CZ_SEVEN_FIVE("gun/cz75"),
+    DESERT_EAGLE("gun/desert_eagle"),
+    DESERT_EAGLE_CLASSIC("gun/desert_eagle_classic");
+
+    private final ResourceLocation modelLoc;
+    private BakedModel cachedModel;
+
+    SpecialModels(String modelName)
+    {
+        this.modelLoc = new ResourceLocation(ProjectArsenal.MOD_ID, "special/" + modelName);
+    }
+
+    public BakedModel getModel() {
+        if (this.cachedModel == null)
+        {
+            this.cachedModel = Minecraft.getInstance().getModelManager().getModel(this.modelLoc);
+        }
+        return this.cachedModel;
+    }
+
+    @SubscribeEvent
+    public static void onRegisterAdditionalModelEvent(final ModelRegistryEvent event)
+    {
+        for (SpecialModels specialModel : values())
+        {
+            ForgeModelBakery.addSpecialModel(specialModel.modelLoc);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onBakingCompletedEvent(final ModelBakeEvent event)
+    {
+        for (SpecialModels specialModel : values())
+        {
+            specialModel.cachedModel = null;
+        }
+    }
+}
